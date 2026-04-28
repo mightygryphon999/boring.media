18c29663e394fc9061ec92b244ae5af00f7276b0
mightygryphon999 <griffin.roberts.srq@gmail.com>
2026-04-28 19:44:49 -0400

    Initial Commit

---
+++ boring.media/boring_mediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  boring_mediaApp.swift
+//  boring.media
+//
+//  Created by Griffin Roberts on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct boring_mediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
